3a233689d0dc25a77c67d948cb209c57ffa218e5
Fikayomi31 <fikayoogundijo@gmail.com>
2024-05-27 03:34:53 -0700

    creating the database in postgresql and improving the models.py file

--- backend/settings.py
+++ backend/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Ahead_system',
+        'USER': 'Ahead_teams',
+        'PASSWORD': 'ahead_pwd',
+        'HOST': 'localhost',  # Set to your PostgreSQL host if it's not localhost
+        'PORT': '5432',       # Default PostgreSQL port
     }
 }
 
